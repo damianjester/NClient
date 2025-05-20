a8581f56725d8198fb4447ded5c98fc4f5523b3a
Damian Jester <damian.jester@proton.me>
2025-05-20 09:51:47 +0200

    Bump compileSdk and targetSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.github.damianjester.nclient"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.github.damianjester.nclient"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
 
         versionCode = 1
         versionName = "0.1.0"
