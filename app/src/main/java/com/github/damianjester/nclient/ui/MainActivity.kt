0f6d9da2e52192a685500036ae7e290d82b975bd
Damian Jester <damian.jester@proton.me>
2025-05-26 11:41:25 +0200

    Remove imports

--- app/src/main/java/com/github/damianjester/nclient/ui/MainActivity.kt
+++ app/src/main/java/com/github/damianjester/nclient/ui/MainActivity.kt
@@ -1,24 +1,19 @@
 package com.github.damianjester.nclient.ui
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.arkivanov.decompose.defaultComponentContext
 import com.github.damianjester.nclient.core.GalleryPageSharer
-import com.github.damianjester.nclient.legacy.settings.Global
 import com.github.damianjester.nclient.ui.theme.NClientTheme
 import com.github.damianjester.nclient.utils.NClientDispatchers
 import kotlinx.coroutines.launch
