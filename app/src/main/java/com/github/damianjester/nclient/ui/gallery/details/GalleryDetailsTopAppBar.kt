7be2a5f7268dc87ccab6ed7d5eada427d8d9a9c6
Damian Jester <damian.jester@proton.me>
2025-05-29 16:51:46 +0200

    Add preview to GalleryDetailsTopAppBar

--- app/src/main/java/com/github/damianjester/nclient/ui/gallery/details/GalleryDetailsTopAppBar.kt
+++ app/src/main/java/com/github/damianjester/nclient/ui/gallery/details/GalleryDetailsTopAppBar.kt
@@ -26,7 +26,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.github.damianjester.nclient.R
+import com.github.damianjester.nclient.ui.theme.NClientPreviewTheme
 
 @Composable
 @OptIn(
@@ -116,4 +119,21 @@ fun GalleryDetailsTopAppBar(
             }
         },
     )
+}
+
+@PreviewLightDark
+@Composable
+private fun GalleryDetailsTopAppBarPreview() {
+    NClientPreviewTheme {
+        GalleryDetailsTopAppBar(
+            galleryTitle = "Hello, World!",
+            isGalleryFavorite = true,
+            gridMode = GalleryDetailsComponent.GridMode.TWO_COLUMNS,
+            onBackClick = {},
+            onTitleLongClick = {},
+            onFavoriteClick = {},
+            onGridModeClick = {},
+            onCommentsClick = {}
+        )
+    }
 }
