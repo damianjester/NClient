e3f0f3cb6d7c8ac7f6d1b1f86c5f6fc3651e9b2c
Damian Jester <damian.jester@proton.me>
2025-06-03 15:22:34 +0200

    Add Material You support

--- app/src/main/java/com/github/damianjester/nclient/ui/theme/Theme.kt
+++ app/src/main/java/com/github/damianjester/nclient/ui/theme/Theme.kt
@@ -1,8 +1,11 @@
 package com.github.damianjester.nclient.ui.theme
 
+import android.os.Build
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
@@ -17,7 +20,10 @@ fun NClientTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
     val colorScheme = when {
+        dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
