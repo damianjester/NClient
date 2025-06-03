b5d327e53ad055d141ba7e13a3d936523267433f
Damian Jester <damian.jester@proton.me>
2025-06-03 17:40:44 +0200

    Enable Coil's GIF support

--- app/src/main/java/com/github/damianjester/nclient/net/NHentaiSingletonImageLoader.kt
+++ app/src/main/java/com/github/damianjester/nclient/net/NHentaiSingletonImageLoader.kt
@@ -1,8 +1,11 @@
 package com.github.damianjester.nclient.net
 
+import android.os.Build
 import coil3.ImageLoader
 import coil3.PlatformContext
 import coil3.SingletonImageLoader
+import coil3.gif.AnimatedImageDecoder
+import coil3.gif.GifDecoder
 import coil3.network.NetworkFetcher
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -14,6 +17,13 @@ class NHentaiSingletonImageLoader : SingletonImageLoader.Factory, KoinComponent 
         return ImageLoader.Builder(context)
             .components {
                 add(factory)
+
+                // Enable Coil's GIF support
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    add(AnimatedImageDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
             }
             .build()
     }
